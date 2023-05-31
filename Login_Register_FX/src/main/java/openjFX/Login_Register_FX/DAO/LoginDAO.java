daa5a3fb6900b6f10de59d7fed5a1c12d1743fb4
ti <thienphuc484@gmail.com>
2023-05-31 12:30:36 +0700

    Add LoginDAO

---
+++ Login_Register_FX/src/main/java/openjFX/Login_Register_FX/DAO/LoginDAO.java
@@ -0,0 +1,30 @@
+package openjFX.Login_Register_FX.DAO;
+
+import openjFX.Login_Register_FX.MySQL.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginDAO {
+    private static final Logger logger = Logger.getLogger(LoginDAO.class.getName());
+
+    public boolean Login (String username,String password){
+        ResultSet resultSet;
+        String loginQuery = "Select * from users where username =? and password =?";
+        try(Connection conn = database.DbConnected();
+            PreparedStatement statement = conn.prepareStatement(loginQuery)){
+            statement.setString(1,username);
+            statement.setString(2,password);
+            resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                logger.info("WELL");
+            }
+        }catch (SQLException e){
+            logger.log(Level.SEVERE,e.getMessage());
+        }
+    return false;
+    }
+}
